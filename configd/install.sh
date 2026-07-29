293205a3e4dad5e98734a4f6563e293c2b70766c
Mathos6 <kanijeremie06@gmail.com>
2026-07-29 01:17:56 +0100

    Modification du script sinstallation

--- configd/install.sh
+++ configd/install.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
 
+
+set -eu
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Lance ce script avec sudo"
+    exit 1
+fi
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-set -e
-sudo apt install libnotify-bin zenity python3 qemu-kvm libguestfs-tools
+for file in quartzine.service 99-my_udev_rules.rules; do
+    [[ -f "$SCRIPT_DIR/$file" ]] || {
+        echo "$file manquant"
+        exit 1
+    }
+done
+
+apt install libnotify-bin zenity \
+    python3 qemu-kvm libguestfs-tools -y
 
 # J'utilise pyudev=0.24.3-1
 # Je n'ai pas encore testé les nouvelles versions (s'il y en a)
-sudo apt install python3-pyudev=0.24.3-1
+# Je modifierai plus tard
+apt install python3-pyudev=0.24.3-1 -y
 
-sudo mkdir -p /opt/Quartzine
-sudo cp 99-my_udev_rules.rules /etc/udev/rules.d
-sudo cp quartzine.service /etc/systemd/system/
+mkdir -p /opt/Quartzine
+cp "${SCRIPT_DIR}"/99-my_udev_rules.rules /etc/udev/rules.d
+cp "${SCRIPT_DIR}"/quartzine.service /etc/systemd/system/
 
 
 # a modifier
-sudo cp -r "$SCRIPT_DIR"/../* /opt/Quartzine/
-echo "Specify the path where you want want to create your virtual disk"
+cp -r "$SCRIPT_DIR"/../. /opt/Quartzine/
 
 
 
 udevadm control --reload-rules
-sudo udevadm trigger
+udevadm trigger
 
-sudo systemctl daemon-reload
-sudo systemctl enable quartzine.service
-sudo systemctl start quartzine.service
-
+systemctl daemon-reload
+systemctl enable --now quartzine.service
