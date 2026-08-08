343fa347c94c63d3435f8279fe6f179e23afa678
Mathieu <164631738+Mathos6@users.noreply.github.com>
2026-08-08 13:41:56 +0100

    Enhance install script with better error handling
    - Fix unbound variable error (USER_HOME) in install.sh by enforcing root check early
    - Automate Alpine base disk download and stream decompression from release asset
    - Handle home directory permissions dynamically for non-root runtime
    - Exclude raw VM storage path from code deployment sync"

--- configd/install.sh
+++ configd/install.sh
@@ -3,8 +3,42 @@
 
 set -eu
 
+if [[ $EUID -ne 0 ]]; then
+    echo "Lance ce script avec sudo"
+    exit 1
+fi
+
+USER_HOME=$(eval echo "~$SUDO_USER")
 IMAGE_DIR="$USER_HOME/.var/quartzine/disk"
 IMAGE_URL="https://github.com/Mathos6/Quartzine/releases/download/vm-v1/alpine.gz"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REAL_USER=${SUDO_USER:-$USER}
+
+
+for file in quartzine.service 99-my_udev_rules.rules; do
+    [[ -f "$SCRIPT_DIR/$file" ]] || {
+        echo "Erreur: $file manquant dans $SCRIPT_DIR"
+        exit 1
+    }
+done
+
+
+apt install libnotify-bin zenity \
+    python3 qemu-kvm libguestfs-tools -y
+
+# J'utilise pyudev=0.24.3-1
+# Je n'ai pas encore testé les nouvelles versions (s'il y en a)
+# Je modifierai plus tard
+apt install python3-pyudev=0.24.3-1 -y
+
+
+mkdir -p /opt/Quartzine
+rsync -av --exclude='vm/disk' "$SCRIPT_DIR"/../. /opt/Quartzine
+
+
+cp "${SCRIPT_DIR}"/99-my_udev_rules.rules /etc/udev/rules.d
+cp "${SCRIPT_DIR}"/quartzine.service /etc/systemd/system/
+
 
 if [ ! -d "$IMAGE_DIR" ] || [ -z "$(ls -A "$IMAGE_DIR" 2>/dev/null)" ]; then
     echo "[+] Dossier VM vide ou inexistant. Téléchargement de l'image Quartzine..."
@@ -16,40 +50,10 @@ if [ ! -d "$IMAGE_DIR" ] || [ -z "$(ls -A "$IMAGE_DIR" 2>/dev/null)" ]; then
     chown -R "$SUDO_USER:$SUDO_USER" "$USER_HOME/.var/quartzine"
 fi
 
-if [[ $EUID -ne 0 ]]; then
-    echo "Lance ce script avec sudo"
-    exit 1
-fi
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-USER_HOME=$(eval echo "~$SUDO_USER")
-
-for file in quartzine.service 99-my_udev_rules.rules; do
-    [[ -f "$SCRIPT_DIR/$file" ]] || {
-        echo "$file manquant"
-        exit 1
-    }
-done
-
-apt install libnotify-bin zenity \
-    python3 qemu-kvm libguestfs-tools -y
-
-# J'utilise pyudev=0.24.3-1
-# Je n'ai pas encore testé les nouvelles versions (s'il y en a)
-# Je modifierai plus tard
-apt install python3-pyudev=0.24.3-1 -y
-
-mkdir -p /opt/Quartzine
-cp "${SCRIPT_DIR}"/99-my_udev_rules.rules /etc/udev/rules.d
-cp "${SCRIPT_DIR}"/quartzine.service /etc/systemd/system/
-
-
-# a modifier
-rsync -av --exclude='vm/disk' "$SCRIPT_DIR"/../. /opt/Quartzine
-mkdir "$USER_HOME"/.var/quartzine
-cp -r "$SCRIPT_DIR"/../vm/disk "$USER_HOME"/.var/quartzine/
+chown -R "$REAL_USER:$REAL_USER" "$USER_HOME/.var/quartzine"
 
 udevadm control --reload-rules
-
 systemctl daemon-reload
 systemctl enable --now quartzine.service
+
+echo "[+] Installation de Quartzine terminée !"
